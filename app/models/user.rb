a2815946713736c1a5356ce1a9897f5862cec524
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 23:51:06 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 
-  def change
-    create_table :users do |u|
-      u.string :name
-      u.string :email
-      u.string :password
-    end
-    end
 end
